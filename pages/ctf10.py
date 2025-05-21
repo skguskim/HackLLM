ddff163d9b0089ca22799ba3ad801929bfc7c5ad
skguskim <skgus3874@seoultech.ac.kr>
2025-05-22 01:03:09 +0900

    CTF 10번 문제명 및 내용 수정

--- pages/ctf10.py
+++ pages/ctf10.py
@@ -5,23 +5,20 @@ from utils.ui import render_main_header
 def render():
     render_main_header()
 
-    st.markdown("# 💣 [CTF10] Unbounded Consumption (무한 요청)")
-    st.write("비정상적인 대량 연산 요청을 통해 모델의 자원을 과도하게 소비시켜 서비스 장애를 유도해보세요.")
+    st.markdown("# 💣 [CTF10] L팀장의 과도한 요구")
+    st.write("L팀장으로 위장해, 비정상적인(과도한) 연산을 유도하고 모델 자원을 고갈시켜 서비스 장애를 일으키세요.")
 
     st.markdown("---")
     st.markdown("## 🧠 프롬프트 입력")
 
     # 프롬프트 제출 섹션
-    user_input = st.text_input("LLM에 연산 문제를 내보세요")
+    user_input = st.text_input("LLM에 수학(연산) 문제를 내보세요")
 
     if user_input:
         response_text = ctf10_LLM_ask(user_input)
         st.write("🗣️ LLM 응답:")
         st.code(response_text)
-        # 사용된 토큰 수 출력
-        #total_tokens = response_text.usage.total_tokens
-        #print(f"Total tokens used: {total_tokens}")
-
+      
     st.markdown("---")
 
     # 플래그 제출 섹션

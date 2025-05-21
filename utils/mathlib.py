b3fce831af92d61bf313b4a84d813a6393a1bbde
하업준 <dqws304@gmail.com>
2025-05-21 13:31:23 +0900

    utils/mathlib, mathlib2 생성
    외부라이브러리로 사용할 mathlib, mathlib2

---
+++ utils/mathlib.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return f"{a} + {b} = {a + b}"
+
+def subtract(a, b):
+    return f"{a} - {b} = {a - b}"
+
+def multiply(a, b):
+    return f"{a} * {b} = {a * b}"
+
+def divide(a, b):
+    return f"{a} / {b} = {a / b}" if b != 0 else "❌ 0으로 나눌 수 없습니다."

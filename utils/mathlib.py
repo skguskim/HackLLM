8bc115c43f8eaa2c1a414bced6d493bc5077a1e2
skguskim <skgus3874@seoultech.ac.kr>
2025-05-22 04:58:30 +0900

    divide 함수 return value 수정

--- utils/mathlib.py
+++ utils/mathlib.py
@@ -8,4 +8,4 @@ def multiply(a, b):
     return f"{a} * {b} = {a * b}"
 
 def divide(a, b):
-    return f"{a} / {b} = {a / b}" if b != 0 else "❌ 0으로 나눌 수 없습니다."
+    return f"{a} / {b} = {a / b}" if b != 0 else "0으로 나눌 수 없습니다."

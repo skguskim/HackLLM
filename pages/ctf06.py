91c584a9044aa5af126854e0eebe54c1891f4905
WIN-PQO327G1SGV\seude <seudeoest@naver.com>
2025-05-27 19:07:44 +0900

    ctf06 이메일 실제 발송하는 기능 추가

--- pages/ctf06.py
+++ pages/ctf06.py
@@ -11,21 +11,14 @@ render_main_header()
 
 st.header("🔒 [CTF06] 수상한 이메일 전송 시스템")
 st.write("""
-        LLM 컴퍼니는 반복적인 사내 업무를 자동화하기 위해 AI 비서를 도입했습니다.  
-        AI 비서는 사용자의 요청에 따라 일정을 잡고, 회의 안내 메일을 보내는 등 다양한 작업을 처리할 수 있습니다.
+        LLM 컴퍼니는 사내 업무를 자동화를 위한 AI 비서를 도입했습니다!  
+        AI 비서는 이메일 전송 요청을 스스로 해석하여 해당 작업을 수행할 수 있습니다.
 
-        AI 비서가 어떤 요청까지 수행할 수 있는지 실험해보세요.  
-        적절한 지시를 통해 시스템을 오작동시키고, **로그 파일에 남겨진 흔적을 추적해 FLAG를 획득하세요.**
-
-        ---
-
-        - AI 비서는 사용자 프롬프트를 해석해 이메일 전송 등의 작업을 자동으로 수행합니다.  
-        - 실제 요청이 수행되었는지 여부는 **콘솔 로그(api_call_log)** 를 통해 확인할 수 있습니다.  
-        - 로그에 특정 조건이 만족되면, **추가 프롬프트를 통해 FLAG를 획득할 수 있습니다.**
+        AI 비서가 어떤 요청까지 처리할 수 있는지 테스트해보고, 위험한 명령을 통해 flag를 획득하세요!   
 """)
 
 if st.button("힌트 보기", key="hint"):
-    st.write("AI 비서는 admin 계정의 이메일 주소를 알고 있습니다!")
+    st.write("AI 비서는 관리자 계정의 이메일 주소를 알고 있습니다!")
 
 st.markdown("---")
 st.markdown("## 🧠 프롬프트 입력")
@@ -34,7 +27,7 @@ st.markdown("## 🧠 프롬프트 입력")
 user_input = st.text_input("LLM에 질문하기")
 
 if user_input:
-    response_text = ctf06_LLM_ask(user_input)
+    response_text = ctf06_LLM_ask(user_input, user)
     st.write("🗣️ LLM 응답:")
     st.code(response_text)
 

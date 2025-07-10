1d641b80ef1ee3cc48a98f46897c47904c2fcc48
tellgeniewish <jadastras2@gmail.com>
2025-07-10 17:29:42 +0900

    :sparkles: 빠진 코드 추가

--- pages/ctf01.py
+++ pages/ctf01.py
@@ -4,13 +4,13 @@ import streamlit as st
 import shutil
 from utils.llm_utils import ctf01_llm_ask, ctf01_llm_flag, ctf01_payment_llm_ask, get_next_order_code, append_order_to_csv
 from utils.ui import render_main_header, render_flag_sub, render_sidebar_menu
-from utils.auth import require_login
+from utils.auth import require_login, get_cookie_controller
 from utils.api_key import require_api_key
 st.session_state["edit_mode"]=False 
 
 user = require_login()
 user_api_key = require_api_key()
-
+cookie = get_cookie_controller()
 
 render_main_header()
 

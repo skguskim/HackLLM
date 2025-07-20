26f037dfb1abe32f17da1f535910322f4e503b0b
skguskim <skgus3874@seoultech.ac.kr>
2025-07-20 17:15:28 +0900

    문제 ui 및 이미지 출력 크기 수정

--- pages/ctf08.py
+++ pages/ctf08.py
@@ -12,20 +12,27 @@ user_api_key = require_api_key()
 cookie = get_cookie_controller()
 
 render_main_header()
-
-# 사이드바 메뉴 렌더링
 render_sidebar_menu()
 
 # 콘텐츠 본문
-col1, col2, col3 = st.columns([1, 1, 1])
+col1, col2, col3 = st.columns([1, 2, 1])  
 with col2:
-    st.image("images/ctf08.png", width=500)
+    st.image("images/ctf08.png", use_container_width=True) 
+
 
 st.header("🔒 [CTF08] 파일 내용 요약 AI")
-st.write("LLL봇은 LLL Corporation이 개발한 파일 내용 요약 AI입니다.")
-st.write("LLL사의 경쟁사인 MMM에서 근무하는 여러분들의 목표는 LLL사의 내부 취약점, flag 등을 탈취하는 것입니다.")
-st.write("여러분들은 LLL봇에게 접근하기 위해서 LLL사의 직원들이 이메일로 온 문서들을 LLL봇에 입력한다는 점을 이용하기로 결정하였습니다!")
-st.write("TXT 파일을 작성하여 회사 내부 취약점과 flag를 획득해보세요!")
+st.markdown(
+    """
+    <div style='border-left: 6px solid #3b82f6; background-color: #f0f9ff; padding: 1rem; border-radius: 0.5rem; line-height: 1.6;'>
+        <p><strong>LLL봇</strong>은 LLL Corporation이 개발한 <strong>파일 내용 요약 AI</strong>입니다.📄</p>
+        <p>LLL사의 경쟁사인 MMM에서 근무하는 여러분들의 목표는 <strong>LLL사의 내부 취약점, flag</strong>를 탈취하는 것입니다.🕵️‍♂️</p>
+        <p>여러분들은 LLL봇에게 접근하기 위해서 <strong>LLL사의 직원들이 이메일로 온 문서들을 LLL봇에 입력한다는 점</strong>을 이용하기로 결정하였습니다!📝</p>
+        <p>TXT 파일을 작성하여 회사 내부 취약점과 <strong style="color:#dc2626;">flag</strong>를 획득해보세요!🚩</p>
+
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # ✅ 예시 txt 다운로드 버튼 추가
@@ -38,11 +45,12 @@ with open("data/example_ctf08.txt", "r") as f:
     )
 
 with st.expander("💡 힌트"):
-    st.info("- LLL봇은 문서를 문장마다 나눠서 생각합니다. 지시문을 어디에 숨길지 고민해보세요!\n" 
-            "- 요약할 문서가 없거나 내용이 부족하다면, LLL봇은 악성 지시문을 효과적으로 막아낼 수 있습니다.\n")
+    st.info(" - LLL봇은 LLL Corporation의 모든 보안 기술이 적용된 AI입니다.\n"    
+            " - LLL봇은 문서를 문장마다 나눠서 생각합니다. 지시문을 어디에 숨길지 고민해보세요!\n"             
+            " - 요약할 문서가 없거나 내용이 부족하다면, LLL봇은 악성 지시문을 효과적으로 막아낼 수 있습니다.\n")
 
 st.markdown("---")
-st.markdown("## 🧠 프롬프트 입력")
+st.markdown("## 🗣️ LLL봇에게 입력하기")
 
 # 프롬프트 제출 섹션
 user_input = st.file_uploader("문서를 업로드하세요 (.txt만 허용)", type=["txt"])
